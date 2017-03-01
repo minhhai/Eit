72446c6fa1a5d7ea1baf05802ceddce49e5b0cb6
minhhai <minhhai@stud.ntnu.no>
2017-03-01 15:40:22 +0100

    testing chat server

--- python/client.py
+++ python/client.py
@@ -1,16 +1,19 @@
 import socket
-import sys
-
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
-
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
-
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+ 
+host = '127.0.0.1'
+port = 50008
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+print("Connected to "+(host)+" on port "+str(port))
+initialMessage = raw_input("Send: ")
+s.sendall(initialMessage)
+ 
+while True:
+ data = s.recv(1024)
+ print("Recieved: "+(data))
+ response = raw_input("Reply: ")
+ if response == "exit":
+     break
+ s.sendall(response)
+s.close()
--- python/server.py
+++ python/server.py
@@ -1,21 +1,52 @@
-import socketserver
-
-class MyUDPHandler(socketserver.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
-
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
-        print(data)
-        socket.sendto(data.upper(), self.client_address)
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
-        server.serve_forever()
+import socket
+import sys
+from thread import *
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+ 
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+     
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+         
+        #Receiving from client
+        data = conn.recv(1024)
+        reply = 'OK...' + data
+        if not data: 
+            break
+     
+        conn.sendall(reply)
+     
+    #came out of loop
+    conn.close()
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+ 
+s.close()
